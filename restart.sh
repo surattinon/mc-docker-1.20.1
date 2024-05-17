76fcc947c9646950f63266d9459072c925a9ea31
surattinon <surattinon.basu@gmail.com>
2024-05-18 06:50:20 +0700

    f

--- restart.sh
+++ restart.sh
@@ -4,5 +4,5 @@ docker exec services-mc-1 rcon-cli say "Saving world..."
 docker exec services-mc-1 rcon-cli save-all
 sleep 15
 docker compose -f ./services/mc-server.yaml down
-sleep 1
+sleep 5
 docker compose -f ./services/mc-server.yaml up -d
