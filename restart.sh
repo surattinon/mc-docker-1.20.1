2ab7d78fd99841eb7e875f4b0a996862b550056c
surattinon <surattinon.basu@gmail.com>
2024-05-22 16:14:38 +0700

    fix restart script

--- restart.sh
+++ restart.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-docker exec services-mc-1 rcon-cli say "=========================="
-docker exec services-mc-1 rcon-cli say "Begin restarting server"
-docker exec services-mc-1 rcon-cli say "=========================="
+docker exec services-mc-1 rcon-cli say "================================="
+docker exec services-mc-1 rcon-cli say "Begin backup and restart server"
+docker exec services-mc-1 rcon-cli say "================================="
 docker exec services-mc-1 rcon-cli save-all
 sleep 5
+docker exec services-mc-1 rcon-cli say ""
+docker exec services-mc-1 rcon-cli say "======================="
+docker exec services-mc-1 rcon-cli say "Begin Backup server..."
+docker exec services-mc-1 rcon-cli say "======================="
+docker compose -f ./services/mc-server.yaml exec backups backup now
 docker exec services-mc-1 rcon-cli say "Server will restart in 15 second"
 sleep 5
 docker exec services-mc-1 rcon-cli say "Server will restart in 10 second"
-docker compose -f ./services/mc-server.yaml exec -d backups backup now
 sleep 5
 docker exec services-mc-1 rcon-cli say "Server will restart in 5 second"
 sleep 5
